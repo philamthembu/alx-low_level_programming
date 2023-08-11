5d9787344c40cc1a1f9757ddb9c713ad00edca2c
philamthembu <philamthembu303@gmail.com>
2023-08-10 23:05:03 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.>
+
+/**
+ * main - prints "prints the size of various types on the computer it
+ *  is compiled and run on."
+ *  Return: always 0.
+ */
+int main(void)
+
+{
+printf("size ofan int: %d byte(s) \n, sizeof(int));
+		return (0);
+}
