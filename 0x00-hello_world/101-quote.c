5d48a0a31047614fd89636d17702d4ad73a957d2
philamthembu <philamthembu303@gmail.com>
2023-08-11 03:19:50 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+/**
+ * main-prints "and that piece of art is useful" - Dora- Korpar, 2015-10-19"
+ * followed by a new line, to the standard error
+ * Return: Always 1
+ */
+
+int main(void)
+{
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\");
+return (1);
+}
