335fcd6c9127c52c98cdbfa8f30e799179196189
philamthembu <philamthembu303@gmail.com>
2023-08-11 01:28:21 -0700

    0processor

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,6 +8,10 @@
 int main(void)
 
 {
-printf("size ofan int: %d byte(s) \n, sizeof(int));
+printf("size ofan int: %zu byte(s) \n", sizeof(int));
+printf("size of a char: %zu bytes(s) \n", sizeof(char));
+printf("size of a long int: %zu byte(s) \n", sizeof(long int));
+printf("size of long long int: %zu byte(s) \n",sizeof(long long int));
+printf("size of a float %zu byte(s) \n",sizeof(float);
 		return (0);
 }
